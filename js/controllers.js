077d46c4db64455e4d06ad6d66e34cefa1d9b23f
jonalling <nginear@gmail.com>
2015-12-31 15:20:34 -0600

    update

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($window, $scope, $ionicModal, $localstorage, $cordovaFile, $ionicPlatform, DataList) {
+.controller('AppCtrl', function($window, $scope, $ionicModal, $localstorage, $cordovaFile, $cordovaEmailComposer, $ionicPlatform, DataList) {
 
   ////// Dims of window, ion-content, and div
   $scope.fullWidth = $window.innerWidth;
@@ -8,13 +8,26 @@ angular.module('starter.controllers', [])
   $scope.width = angular.element(document.querySelector('#main'))[0].offsetWidth;
   $scope.height = angular.element(document.querySelector('#main'))[0].offsetHeight;
 
+  $scope.dataList = DataList;
+
   //// RETRIEVE LOCAL DATA //////
   $scope.loadData = function() {
     // update DeviceData with localStorage
+
     DataList = $localstorage.getObject('storedDataList');
+
+    if (DataList !== 'undefined' && DataList !== null) {
+      $localstorage.setObject('storedDataList', {
+        data: []
+      });
+    }
   }
 
   $scope.loadData();
+
+  console.log(JSON.stringify(DataList.data));
+
+
 
   ////// FORM //////
   // $scope.list = [];
@@ -42,17 +55,64 @@ angular.module('starter.controllers', [])
   }
 
   // based on https://github.com/apache/cordova-plugin-file
+  // also see this for email concept... http://stackoverflow.com/questions/31931173/how-to-attach-file-to-email-in-ionic-framework-android
+  // Could also use $http (see commented code below)
   $scope.dumpData = function() {
     $ionicPlatform.ready(function(){
-      $cordovaFile.writeFile(cordova.file.syncedDataDirectory, "list.json", JSON.stringify($scope.list), true)
+      $cordovaFile.writeFile(cordova.file.syncedDataDirectory, "list.json", JSON.stringify(DataList.data), true)
       .then(function (success) {
         console.log("data dumped")
+        $scope.closeSettings();
+        $cordovaEmailComposer.isAvailable().then(function() {
+          // is available
+          console.log('email started')
+          var email = {
+            to: 'nginear@gmail.com',
+            attachments: ['' + cordova.file.syncedDataDirectory.replace('file://','') + "list.json"],
+            subject: 'Data Dump',
+            body: 'Json data',
+            isHtml: false
+          };
+
+          $cordovaEmailComposer.open(email).then(null, function () {
+          // user cancelled email
+          });
+          console.log('email made')
+          return;
+          }, function (error) {
+          return;
+        });
         // success
       }, function (error) {
+        console.log("error")
         // error
       });
     });
   }
+
+  // $scope.go = function() {
+  //
+  // 	var req = {
+  // 		method: 'POST',
+  // 		url: 'https://api-http.littlebitscloud.cc/devices/' + CLOUDBIT_ID + '/output',
+  // 		headers: {
+  // 			'Authorization': 'Bearer ' + ACCESS_TOKEN,
+  // 			'Accept': 'application/vnd.littlebits.v2+json',
+  // 			'Content-Type': 'application/json'
+  // 		},
+  // 		data: JSON.stringify({
+  //       'percent': 10,
+  //       'duration_ms': 500
+  //     })
+  // 	}
+  //
+  // 	$http(req).success(function(data) {
+  //     $scope.hello = data;
+  // 		console.log($scope.hello);
+  // 		$scope.startTimer();
+  //   })
+  //
+  // }
 
   ////// MODAL //////
   $ionicModal.fromTemplateUrl('templates/settings.html', {
